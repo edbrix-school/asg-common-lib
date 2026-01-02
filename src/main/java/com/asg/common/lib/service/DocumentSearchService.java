15e11114eedf767f5e4f947deb612ee10d4a23b2
Ajith Kumar <aandiappan@hexalytics.com>
2026-01-02 12:46:15 +0530

    Fix for Search API

--- src/main/java/com/asg/common/lib/service/DocumentSearchService.java
+++ src/main/java/com/asg/common/lib/service/DocumentSearchService.java
@@ -50,9 +50,11 @@ public class DocumentSearchService {
 //        System.out.println("Displayable Columns for docId=" + docId);
 //        displayCols.forEach((k, v) -> System.out.println("   " + k + " -> " + v));
 
-        String baseSql = !doc.getListOfRecordsSql().isBlank()
+        String rawBaseSql = !doc.getListOfRecordsSql().isBlank()
                 ? cleanSql(doc.getListOfRecordsSql())
                 : "SELECT * FROM " + doc.getMainTableName();
+
+        String baseSql = "SELECT * FROM (" + rawBaseSql + ") BASE";
 
         List<String> columnNames = getSearchableFieldNames(doc);
 
