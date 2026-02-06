f284ff6950ce6135dff91dd8c7880bd9f331831d
Gouri Sanjay Jadhav <gourij@hexalytics.com>
2026-02-06 16:52:56 +0530

    added detrowid in dto

--- src/main/java/com/asg/common/lib/dto/UserRoleDto.java
+++ src/main/java/com/asg/common/lib/dto/UserRoleDto.java
@@ -6,6 +6,6 @@ import java.sql.Date;
 
 @Builder
 public record UserRoleDto(Long userRolePoId, String userRoleId, String userRoleName, Date expiryDate, String deleted,
-                          String active, String actionType) {
+                          String active, String actionType,Long detRowId) {
 }
 
