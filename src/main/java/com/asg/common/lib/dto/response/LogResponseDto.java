bee02eff60902d4aa3dbbae32f1471132922bac7
Sahil Khurana <skhurana@hexalytics.com>
2026-02-16 10:00:15 +0530

    Fixed Datetime in Logging

--- src/main/java/com/asg/common/lib/dto/response/LogResponseDto.java
+++ src/main/java/com/asg/common/lib/dto/response/LogResponseDto.java
@@ -4,13 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LogResponseDto {
-    private Timestamp logDateTime;
+    private LocalDateTime logDateTime;
     private String userName;
     private Long logUserPoid;
     private String logDetails;
---
+++ src/main/java/com/asg/common/lib/utility/DateUtil.java
@@ -0,0 +1,26 @@
+package com.asg.common.lib.utility;
+
+import com.asg.common.lib.security.util.UserContext;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public final class DateUtil {
+
+    private DateUtil() {
+    }
+
+    public static LocalDateTime getCurrentDateTimeInUserTimeZone() {
+        String timeZoneCode = UserContext.getTimeZoneCode();
+        ZoneId zoneId = ZoneId.of(timeZoneCode);
+        return ZonedDateTime.now(zoneId).toLocalDateTime();
+    }
+
+    public static Timestamp getCurrentDateTimeInUserTimeZoneTimeStamp() {
+        String timeZoneCode = UserContext.getTimeZoneCode();
+        ZoneId zoneId = ZoneId.of(timeZoneCode);
+        return Timestamp.valueOf(ZonedDateTime.now(zoneId).toLocalDateTime());
+    }
+}
