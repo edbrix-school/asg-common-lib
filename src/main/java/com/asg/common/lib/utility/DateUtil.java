f5104a298b5de0d7c5083addca52f8af0d2a57ab
Sahil Khurana <skhurana@hexalytics.com>
2026-02-18 13:40:43 +0530

    Timezone Changes for Settings Module

--- src/main/java/com/asg/common/lib/utility/DateUtil.java
+++ src/main/java/com/asg/common/lib/utility/DateUtil.java
@@ -3,6 +3,7 @@ package com.asg.common.lib.utility;
 import com.asg.common.lib.security.util.UserContext;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -16,6 +17,12 @@ public final class DateUtil {
         String timeZoneCode = UserContext.getTimeZoneCode();
         ZoneId zoneId = ZoneId.of(timeZoneCode);
         return ZonedDateTime.now(zoneId).toLocalDateTime();
+    }
+
+    public static LocalDate getCurrentDateInUserTimeZone() {
+        String timeZoneCode = UserContext.getTimeZoneCode();
+        ZoneId zoneId = ZoneId.of(timeZoneCode);
+        return ZonedDateTime.now(zoneId).toLocalDate();
     }
 
     public static Timestamp getCurrentDateTimeInUserTimeZoneTimeStamp() {
