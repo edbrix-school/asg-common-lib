b49d35f5cca7125a03dc710af135a30696e0d429
AKhilesh-Dwivedi <adwivedi@hexalytics.com>
2026-03-02 15:48:07 +0530

    fix date time issue

--- src/main/java/com/asg/common/lib/dto/request/BillwiseBreakupRequestDto.java
+++ src/main/java/com/asg/common/lib/dto/request/BillwiseBreakupRequestDto.java
@@ -6,7 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDate;
+
 
 @Data
 @NoArgsConstructor
@@ -23,7 +24,7 @@ public class BillwiseBreakupRequestDto {
     private Long billDetRowId;
     private String billRefType;
     private String billRef;
-    private Date billDueDate;
+    private LocalDate billDueDate;
     private BigDecimal drAmt;
     private BigDecimal crAmt;
     private String billRemarks;
