cadcbd9dc737c8965151e00ff4639ee210adbc76
Vaikundamuthu <vkannan@hexalytics.com>
2026-03-18 22:04:39 +0530

    Added BENEFICIARY_NAME into skip filter list

--- src/main/java/com/asg/common/lib/service/LovDataService.java
+++ src/main/java/com/asg/common/lib/service/LovDataService.java
@@ -29,7 +29,8 @@ public class LovDataService {
 
     private static final List<String> SKIP_FILTER_LOV_NAMES = Arrays.asList(
             "TERMS_TEMPLATE_MASTER",
-            "CHQ_RETURN_RECEIPT_NO"
+            "CHQ_RETURN_RECEIPT_NO",
+            "BENEFICIARY_NAME"
     );
 
 
