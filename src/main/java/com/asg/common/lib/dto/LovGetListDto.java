2e9ea14b41249144a6c6d70a2835c4008fbb5066
Karthikhexalytics <kgunasekar@hexalytics.com>
2026-04-08 11:44:44 +0530

    Added a parameterized constructor for the LovGetListDto

--- src/main/java/com/asg/common/lib/dto/LovGetListDto.java
+++ src/main/java/com/asg/common/lib/dto/LovGetListDto.java
@@ -9,14 +9,34 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LovGetListDto {
+
     private Long poid;
     private String code;
     private String label;
     private Long value;
     private String description;
     private Integer seqNo;
+
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String users;
+
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String controlAcNature;
+
+    public LovGetListDto(Long poid,
+                         String code,
+                         String label,
+                         Long value,
+                         String description,
+                         Integer seqNo,
+                         String users) {
+        this.poid = poid;
+        this.code = code;
+        this.label = label;
+        this.value = value;
+        this.description = description;
+        this.seqNo = seqNo;
+        this.users = users;
+    }
 }
+
